cf90b165285454dd84849c1a468249f1cf6e243a
shim079 <sh.s.alsuraihi@gmail.com>
2026-05-08 00:17:41 +0300

    Added certificate PDF download feature

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from .views import certificate_detail, certificate_detail_ar, my_certificates
+from . import views
 
 urlpatterns = [
     path('certificate/<int:certificate_id>/', certificate_detail, name='certificate_en'),
     path('certificate/ar/<int:certificate_id>/', certificate_detail_ar, name='certificate_ar'),
     path('my-certificates/', my_certificates, name='my_certificates'),
+    path(
+    'certificate/<int:certificate_id>/pdf/',
+    views.download_certificate_pdf,
+    name='download_certificate_pdf'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,8 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Certificate
 from accounts.models import Notification
-
+from django.template.loader import get_template, render_to_string
+from xhtml2pdf import pisa
 
 @login_required
 def my_certificates(request):
@@ -29,3 +31,31 @@ def certificate_detail_ar(request, certificate_id):
     return render(request, 'users/certificate_detail_ar.html', {
         'certificate': certificate
     })
+
+
+def download_certificate_pdf(request, certificate_id):
+
+    certificate = Certificate.objects.get(id=certificate_id)
+
+    template = get_template(
+        'users/certificate_detail_ar.html'
+    )
+
+    html = template.render({
+        'certificate': certificate
+    })
+
+    response = HttpResponse(
+        content_type='application/pdf'
+    )
+
+    response['Content-Disposition'] = (
+        f'attachment; filename="certificate_{certificate.id}.pdf"'
+    )
+
+    pisa.CreatePDF(
+        html,
+        dest=response
+    )
+
+    return response
